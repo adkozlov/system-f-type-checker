28a1232f45ebf99fe0846c02d56048ffbb438ea4
Andrew Kozlov <adkozlov@gmail.com>
2015-12-21 16:56:46 +0300

    additional tests

--- src/test/scala/ru/spbau/kozlov/typechecker/TypeCheckerTest.scala
+++ src/test/scala/ru/spbau/kozlov/typechecker/TypeCheckerTest.scala
@@ -44,13 +44,108 @@ class TypeCheckerTest {
 
   @Test
   def testSelfApp() {
-    val `type` = forAllX(X_TO_X_TYPE)
-    assertEquals(->(`type`, `type`), check(xAbstraction(`type`, Application(TypeApplication(X_TERM, `type`), X_TERM))))
+    val top = forAllX(X_TO_X_TYPE)
+    assertEquals(->(top, top), check(xAbstraction(top, Application(TypeApplication(X_TERM, top), X_TERM))))
+  }
+
+  @Test
+  def testSelfApp2() {
+    val bottom = forAllX(X_TYPE)
+    val bottomToBottomType = ->(bottom, bottom)
+    assertEquals(bottomToBottomType, check(
+      xAbstraction(bottom,
+        Application(
+          TypeApplication(X_TERM, bottomToBottomType),
+          X_TERM))))
+  }
+
+  @Test
+  def testSelfApp3() {
+    val bottom = ∀("Y", ->(TypeVariable("Y"), TypeVariable("Y")))
+    assertEquals(->(bottom, bottom), check(
+      xAbstraction(
+        bottom,
+        xTypeAbstraction(
+          Application(
+            TypeApplication(X_TERM, X_TO_X_TYPE),
+            TypeApplication(X_TERM, X_TYPE))))))
+  }
+
+  @Test
+  def testSelfApp4() {
+    val top = ∀("Y", ->(TypeVariable("Y"), TypeVariable("Y")))
+    assertEquals(->(top, top), check(
+      xAbstraction(
+        top,
+        xTypeAbstraction(
+          Application(
+            TypeApplication(X_TERM, X_TO_X_TYPE),
+            TypeApplication(X_TERM, X_TYPE))))))
   }
 
   @Test(expected = classOf[TypeCheckerException])
   def testUnknownVariable() {
     check(X_TERM)
+  }
+
+  @Test
+  def testBottom() {
+    val bottom = forAllX(X_TYPE)
+    val f = TermVariable("f")
+    assertEquals(->(bottom, bottom), check(
+      Abstraction(
+        "f",
+        bottom,
+        Application(
+          Application(
+            TypeApplication(f, ->(bottom, ->(bottom, bottom))),
+            TypeApplication(f, bottom)),
+          TypeApplication(f, bottom)))))
+  }
+
+  @Test
+  def testBottom2() {
+    val bottom = forAllX(X_TYPE)
+    val f = TermVariable("f")
+    assertEquals(->(bottom, bottom), check(
+      Abstraction(
+        "f",
+        bottom,
+        Application(
+          Application(
+            TypeApplication(f, ->(->(bottom, bottom), ->(bottom, bottom))),
+            TypeApplication(f, ->(bottom, bottom))),
+          TypeApplication(f, ->(bottom, bottom))))))
+  }
+
+  @Test
+  def testInference() {
+    val yType = TypeVariable("Y")
+    val term = TypeApplication(
+      xTypeAbstraction(
+        TypeAbstraction(
+          "Y",
+          xAbstraction(
+            X_TYPE,
+            Abstraction("y", yType, X_TERM)))),
+      yType)
+    assertEquals(∀("Y", ->(yType, ->(yType, yType))), check(term, Context(List(TypeAssumption("Y")))))
+  }
+
+  @Test
+  def testInference2() {
+    val yType = TypeVariable("Y")
+    val term = Application(
+      TypeApplication(
+        xTypeAbstraction(
+          xAbstraction(
+            X_TYPE,
+            TypeAbstraction(
+              "Y",
+              Abstraction("y", yType, X_TERM)))),
+        yType),
+      TermVariable("y"))
+    assertEquals(∀("Y", ->(yType, yType)), check(term, Context(List(TypeAssumption("Y"), TermAssumption("y", yType)))))
   }
 }
 
